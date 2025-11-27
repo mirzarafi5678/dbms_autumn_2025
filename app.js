bd57011c3f263eb3f7a63b8099e242c462fbe0fb
mirzarafi5678 <mirzasufianrafi5678@gmail.com>
2025-11-27 10:52:38 +0600

    .

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.use(
 
 app.get('/logout', (req, res) => {
     req.session.destroy(err => {
+         
         if (err) console.log(err);
+        console.log("goodbye")
         res.redirect('/login');
     });
 });
